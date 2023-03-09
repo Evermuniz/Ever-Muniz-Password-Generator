a53884d28be35fe3bea7bf30c7ac0fe75a73a24b
evermuniz <evermuniz@outlook.com>
2023-03-08 21:18:10 -0700

    added for loop using prompt response

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -4,11 +4,12 @@ var upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O
 var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numericArray = ["0","1","2","3","4","5","6","7","8","9"];
 var specialCharacterArray = ["!", "@", "#", "$", "%", "^", "&", "*",];
+var passwordArray = [];
 
 // Write password to the #password input
 function writePassword() {
-  // var length = prompt("How many characters would you like in your password? Choose between 8 and 128.")
-  //   console.log(length);
+  var length = prompt("How many characters would you like in your password? Choose between 8 and 128.")
+    console.log(length);
 
   var lowerCase = prompt("Would you like your password to contain lower case letters? Enter Y or N")
     console.log(lowerCase);
@@ -22,22 +23,31 @@ function writePassword() {
   var specialCharacter = prompt("Would you like your password to contain special characters? Enter Y or N")
     console.log(specialCharacter);
 
+
+  
+  if (upperCase === "Y")
+  for (var i=0; i<length; i++){
   var index = Math.floor(Math.random() * upperCaseArray.length)
   var a = upperCaseArray[index];
 
+  if (lowerCase === "Y")
   var index = Math.floor(Math.random() * lowerCaseArray.length)
   var b = lowerCaseArray[index];
 
+  if (numeric === "Y")
   var index = Math.floor(Math.random() * numericArray.length)
   var c = numericArray[index];
 
+  if (specialCharacter === "Y")
   var index = Math.floor(Math.random() * specialCharacterArray.length)
   var d = specialCharacterArray[index];
 
+  passwordArray.push(a+b+c+d)}
 
-  function generatePassword() {
-    return a + b + c +d;
-};
+
+  function generatePassword(){ 
+    return passwordArray;
+  };
 
 
   var password = generatePassword();
