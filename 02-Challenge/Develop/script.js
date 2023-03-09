ffef2aa368724fcb9f8e74181f08725daca10257
evermuniz <evermuniz@outlook.com>
2023-03-08 19:34:11 -0700

    added arrays

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,7 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-
+var specialCharacterArray = ["!", "@", "#", "$", "%", "^", "&", "*",];
+var upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 // Write password to the #password input
 function writePassword() {
@@ -17,19 +18,23 @@ function writePassword() {
   var numeric = prompt("Would you like your password to contain numbers? Enter Y or N")
     console.log(numeric);
 
-  var specialCharacters = prompt("Would you like your password to contain special characters? Enter Y or N")
-    console.log(specialCharacters);
+  var specialCharacter = prompt("Would you like your password to contain special characters? Enter Y or N")
+    console.log(specialCharacter);
 
+  var index = Math.floor(Math.random() * specialCharacterArray.length)
+  var computer = specialCharacterArray[index];
+
+  if (specialCharacter ==="Y") {window.alert(computer);}
   
+
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
   
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
 
 
-  passwordText.value = password;
+  // passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
