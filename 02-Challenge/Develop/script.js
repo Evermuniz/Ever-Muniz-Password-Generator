78ec0f478369939f40274792e4fa593004b5d12b
evermuniz <evermuniz@outlook.com>
2023-03-08 20:31:04 -0700

    generatePassword function defined

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,13 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specialCharacterArray = ["!", "@", "#", "$", "%", "^", "&", "*",];
 var upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numericArray = ["0","1","2","3","4","5","6","7","8","9"];
+var specialCharacterArray = ["!", "@", "#", "$", "%", "^", "&", "*",];
 
 // Write password to the #password input
 function writePassword() {
-  var length = prompt("How many characters would you like in your password? Choose between 8 and 128.")
-    console.log(length);
+  // var length = prompt("How many characters would you like in your password? Choose between 8 and 128.")
+  //   console.log(length);
 
   var lowerCase = prompt("Would you like your password to contain lower case letters? Enter Y or N")
     console.log(lowerCase);
@@ -21,18 +22,34 @@ function writePassword() {
   var specialCharacter = prompt("Would you like your password to contain special characters? Enter Y or N")
     console.log(specialCharacter);
 
+  var index = Math.floor(Math.random() * upperCaseArray.length)
+  var a = upperCaseArray[index];
+
+  var index = Math.floor(Math.random() * lowerCaseArray.length)
+  var b = lowerCaseArray[index];
+
+  var index = Math.floor(Math.random() * numericArray.length)
+  var c = numericArray[index];
+
   var index = Math.floor(Math.random() * specialCharacterArray.length)
-  var computer = specialCharacterArray[index];
+  var d = specialCharacterArray[index];
 
-  if (specialCharacter ==="Y") {window.alert(computer);}
+
+  function generatePassword() {
+    return a + b + c +d;
+};
+
+
+  var password = generatePassword();
+
+
+
+
+  var passwordText = document.querySelector("#password");
   
 
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-  
 
-
-  // passwordText.value = password;
+  passwordText.value = password;
 
 };
 
