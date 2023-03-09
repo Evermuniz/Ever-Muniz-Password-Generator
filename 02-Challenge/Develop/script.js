c11ae40f2cc6a30d51a5f44fd578b65abf381cf0
evermuniz <evermuniz@outlook.com>
2023-03-09 11:53:42 -0700

    added ability to cancel function

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -8,27 +8,30 @@ var passwordArray = [];
 
 // Write password to the #password input
 function writePassword() {
+
   
   var length = prompt("How many characters would you like in your password? Choose between 8 and 128.");
-    console.log(length);
+    if (!length){return};
 
   if (length < 8 ||length>128) {
   var tryAgain = window.confirm("Out of range. Try again?")
     if (tryAgain){
       writePassword();
-    }};
+    }
+    else {return};
+  };
   
   var lowerCase = prompt("Would you like your password to contain lower case letters? Enter Y or N")
-    console.log(lowerCase);
+    if (!lowerCase){return};
 
   var upperCase = prompt("Would you like your password to contain upper case letters? Enter Y or N")
-    console.log(upperCase);
+    if (!upperCase){return};
 
   var numeric = prompt("Would you like your password to contain numbers? Enter Y or N")
-    console.log(numeric);
+    if (!numeric){return};
 
   var specialCharacter = prompt("Would you like your password to contain special characters? Enter Y or N")
-    console.log(specialCharacter);
+    if (!specialCharacter){return};
 
   
   if (upperCase === "Y" || "y'")
