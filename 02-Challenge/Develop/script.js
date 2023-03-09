383922cbb35f6e99e99ba6509627d786875c6552
evermuniz <evermuniz@outlook.com>
2023-03-08 19:03:27 -0700

    added initial prompts

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,10 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
+  var length = prompt("How many characters would you like in your password? Choose between 8 and 128.")
+    console.log(length);
+
+  var lowerCase = prompt("Would you like your password to contain lower case letters? Enter Y or N")
+    console.log(lowerCase);
+
+  var upperCase = prompt("Would you like your password to contain upper case letters? Enter Y or N")
+    console.log(upperCase);
+
+  var numeric = prompt("Would you like your password to contain numbers? Enter Y or N")
+    console.log(numeric);
+
+  var specialCharacters = prompt("Would you like your password to contain special characters? Enter Y or N")
+    console.log(specialCharacters);
+
+  
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
+
 
   passwordText.value = password;
 
@@ -12,3 +33,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
