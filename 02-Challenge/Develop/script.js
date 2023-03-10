21e25f6b1bdd753df090f454519508b8fa45aa18
evermuniz <evermuniz@outlook.com>
2023-03-09 21:49:07 -0700

    added loop for prompts

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -4,6 +4,7 @@ var upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O
 var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numericArray = ["0","1","2","3","4","5","6","7","8","9"];
 var specialCharacterArray = ["!", "@", "#", "$", "%", "^", "&", "*",];
+var options = ["Y", "N", "y", "n"];
 
   
 
@@ -20,6 +21,11 @@ function writePassword() {
     if (tryAgain){
       writePassword();
     }
+  if (!numericArray.includes(length)){
+    var tryAgain = window.confirm("Out of range. Try again?")
+    if (tryAgain){
+      writePassword();
+  }
     else {return};
   };
 
@@ -27,20 +33,29 @@ do{
   var upperCase = prompt("Would you like your password to contain upper case letters? Enter Y or N")
   if (!upperCase){return};
   upperCase = upperCase.toUpperCase();
-  if (upperCase != "Y") window.alert ("Invalid");}
-  while (upperCase != "Y"); 
+  if (!options.includes(upperCase)) window.alert ("Invalid");}
+  while (!options.includes(upperCase));
  
+  do{
   var lowerCase = prompt("Would you like your password to contain lower case letters? Enter Y or N")
     if (!lowerCase){return};
     lowerCase = lowerCase.toUpperCase();
+   if (!options.includes(lowerCase)) window.alert ("Invalid");}
+  while (!options.includes(lowerCase));
 
+  do {  
   var numeric = prompt("Would you like your password to contain numbers? Enter Y or N")
     if (!numeric){return};
     numeric = numeric.toUpperCase();
+  if (!options.includes(numeric)) window.alert ("Invalid");}
+  while (!options.includes(numeric));
 
+  do {  
   var specialCharacter = prompt("Would you like your password to contain special characters? Enter Y or N")
     if (!specialCharacter){return};
     specialCharacter = specialCharacter.toUpperCase();
+    if (!options.includes(specialCharacter)) window.alert ("Invalid");}
+  while (!options.includes(specialCharacter));
 
   if (upperCase != "Y" && lowerCase != "Y" && numeric != "Y" && specialCharacter != "Y" ){
   var selectOne = window.confirm("You must select at least one class of characters. Try again?")
